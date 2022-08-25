39ce4cb8888da90e3a415e5165e4e46e7b735aee
jaikdm <jaikdm95@gmail.com>
2022-08-25 14:46:02 +0530

    commit by kk

--- RepoDemo/RepoDemo/src/main/java/com/csi/controller/RepoController.java
+++ RepoDemo/RepoDemo/src/main/java/com/csi/controller/RepoController.java
@@ -12,4 +12,9 @@ public class RepoController {
     public String sayHello(){
         return "Shraddha's repository :)";
     }
+
+    @GetMapping("/kk")
+    public String changeByKartik(){
+        return "Hello, How are you ...? :)";
+    }
 }
